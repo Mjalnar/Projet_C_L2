f5754d1bd88a2b2ed07c11be2b4df65904779e36
Mjalnar <117657216+Mjalnar@users.noreply.github.com>
2023-12-01 09:54:53 +0100

    Update list.h

--- list.h
+++ list.h
@@ -17,7 +17,6 @@ list* add_val_to_head(list *mylist, cell*);
 void print_all_list(list* mylist);
 void print_lvl_list(list* mylist, int nb_lvl);
 list* tri_liste_croisant(list *mylist, int nb_lvl, int val);
-list* creer_liste_pour_n(int n);
 
 
 #endif //PROJET_LIST_H
